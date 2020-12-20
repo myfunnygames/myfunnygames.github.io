51795fdff752f6e07490ba656b2885309b92b891
myfunnygames <72666775+myfunnygames@users.noreply.github.com>
2020-12-20 16:25:22 +0100

    Create tasks.js

---
+++ tasks.js
@@ -0,0 +1,179 @@
+let icons = {
+    rabbit: '🐰',
+    cat: '🐱',
+    lion: '🦁'
+};
+
+let tasks = {
+    l1: {
+        rabbit: [
+            'The other players have to ask you questions about four hidden cards. You have to take a sip for every wrong guess.',
+            'All players who follow their ex on Instagram have to take a sip.',
+            'Show the other players a really embarrassing picture of yourself.',
+            'All players have to name countries in the east of Asia. The first player not able to name one has to take a sip.',
+            'Have a short conversation with a chair in the room as if it was answering!',
+            'The other players can mix you a shot of any beverages and you have to drink it!',
+            'What embarrassing thing do you sometimes do in front of the mirror?',
+            'Talk about your very first crush!'
+        ],
+        cat: [
+            'Any player who has never written a love letter to someone has to take a sip.',
+            'Where on your body would you put a tattoo, if you had to?',
+            'Try to get your feet behind your head. If you succeed, you can assign a sip to another player.',
+            'Demonstrate your cat-walk abilities to everybody! Act like a model!',
+            'Every player has to draw a card. The player with the highest value can assign a sip to another player.',
+            'Play the last song you heard on your phone out loud and dance to it wildly!',
+            'For the next 3 rounds, whoever holds a glass with the dominant hand has to take an extra sip.',
+            'Any player who has not kissed someone older and younger than them has to take a sip.'
+        ],
+        lion: [
+            'Every player has to draw a card. The player with the lowest value has to take a sip.',
+            'Quickly draw a picture of your dream partner!',
+            'Do you always wash your hands after you have been to the toilet? Be honest!',
+            'The other players have to ask you questions about four hidden cards. You can assign a sip for every right guess.',
+            'Make a short rap performance without music!',
+            'You have to talk with a strong accent for one round. If you break it, you have to drink.',
+            'Pose like a bodybuilder in front of a jury!',
+            'What is more important on a girl, the boobs or the butt? What do you look at first?'
+        ]
+    },
+    l2: {
+        rabbit: [
+            'Challenge player 🐱 for a short game of beer pong!',
+            'What do you find most attractive and sexy about yourself? Tell the others!',
+            'Any player who has been woken up by someone because of their snoring has to take a sip.',
+            'Feed player 🦁 carefully with a few bites of food!',
+            'Imitate a street hooker who tries to get another player to buy her!',
+            'Any player who never pees in the shower has to take a big sip.',
+            'The other players have to guess the color of your panties. Either you have to take a sip or assign a sip.',
+            ['Would you kiss a girl?', 'If not for free, for how much money would you do it?'],
+            'The other players get to tickle you for 10 seconds. If you laugh, you have to take a sip.'
+        ],
+        cat: [
+            'Talk about an embarrassing situation that you found yourself in!',
+            'Lick a non-food-related object in this room!',
+            'Describe your most recurring dream!',
+            'The other players have to guess your favorite porn category. Either you have to take a sip or assign a sip.',
+            'Describe your dream job without using the name!',
+            'On your own, demonstrate a passionate french kiss to others!',
+            'Is sex or love more important to you in a relationship?',
+            'Demonstrate how you would try to pick up player 🐰 in a bar! Act out a short scene!',
+            'Tell a secret that your parents can never know!',
+        ],
+        lion: [
+            'Let the other players pluck your eyebrows!',
+            'Have you ever been rejected by a love interest? Be honset!',
+            'The other players have to assist you at acting as if you were their dog!',
+            'Have you ever stolen money from your parents? Tell about it!',
+            'You and player 🐱 are now drinking partners for 3 rounds. Whenever one has to take a sip, so does the other.',
+            'Any player who has never bought drugs has to take a sip.',
+            'Explain to the others what gets you into the mood for sex!',
+            ['Reenact a scene from a movie! The other players have to guess the origin.', 'If a player recognized the movie, you can assign a sip.'],
+            'Take a small sip for every time you have measured your penis.',
+        ]
+    },
+    l3: {
+        rabbit: [
+            'For the next 3 rounds, whoever says "drink!" in any language has to take a sip.',
+            'Tell the others about very embarrassing situation you have been in!',
+            'Choose one player to spank your butt!',
+            ['Have you been in love with someone for months without getting something in return? Tell the story!', 'If you told a story, you can assign a sip to someone.'],
+            'Who of the other players would you bite if you were a hungry vampire?',
+            'Any player who has never searched for dickpics on the internet has to take a sip.',
+            'What would you do if you do and try if you only had one more day to live?'
+        ],
+        cat: [
+            'Who of the other players would you want to see naked, if you had to choose one?',
+            'Would you break up with a man once you learn that he has a too small penis?',
+            'Drink if you have ever french-kissed a girl!',
+            'Reenact the famous orgasm scene from "When Harry Met Sally"',
+            'Draw a card and take a number of small sips, according to its value!',
+            'With whom have you had the best sex of your life yet? Be honest!',
+            'The player who was the oldest when they lost their virginity has to take a big sip. However, it can be shared with a virgin.',
+            'You and player 🦁 get 2 minutes alone in this room. The other players are not allowed to ask what happened.'
+        ],
+        lion: [
+            ['Imitate the sounds of three animals! The other players have to recognize them.', 'If not all were recognized, you have to take a sip.'],
+            'Challenge player 🐰 to a game of arm wrestling! The loser has to take a sip.',
+            'All female players have to take a sip for every day since they shaved their legs.',
+            'Spray whipped cream into player 🐱 mouth in the kinkiest way you can think of!',
+            'All the players who never had sex or foreplay through a phone or computer have to take a sip.',
+            'Show the other players a funny porn video intro you have seen recently!',
+            'Whisper kinky things into player 🐰 ear! Try to cause player 🐰 to blush!',
+            'For the next 3 rounds, before a player takes a sip, they have to imitate an engine sound.'
+        ]
+    },
+    l4: {
+        rabbit: [
+            'Any player who has not peed their pants in recent years has to take a sip. The others have to tell their story.',
+            'Dress up as an animal and imitate its behavior.',
+            'What is the sexiest piece of underwear you own? Who do you want to see you in it?',
+            'Choose one player to spank your butt!',
+            'Which sexual practice are you curious to try?',
+            'Wear one of your panties on your forehead for one round! Wear it like a crown!',
+            'All players whose bra and panties do not have matching colors have to take a sip.',
+            'Explain to the other players what a partner has to do to turn you on!',
+            'Play the last song you heard on your phone out loud and dance to it wildly!'
+        ],
+        cat: [
+            'Tell the other players at which place closest to the game you had sex recently!',
+            'Drink if you have ever french-kissed a girl!',
+            'Would you break up with a man once you learn that he has a too small penis?',
+            'Demonstrate your blow job technique to the other players with a fruit!',
+            'Draw a picture of player 🦁\'s penis',
+            'Which sexual practice are you curious to try?',
+            'Change one piece of your current clothing with one of player 🐰!',
+            'Any player who has not yet masturbated this week has to take a big sip.',
+            'Search for and present a porn video that you think player 🦁 would enjoy!',
+        ],
+        lion: [
+            'Tell the other players how you take care of your pubic hair!',
+            'Spray whipped cream into player 🐱 mouth in the kinkiest way you can think of!',
+            'What was the strangest or most embarrassing thing to happen to you in a sexual context?',
+            'Show the other players a really embarrassing picture of yourself.',
+            'All players have to take a sip for each bra size they have. (A=1, B=2, ...). Men included.',
+            'Players who have never been caught during sex have to take a sip. The others have to tell their story.',
+            'Demonstrate your favourite sex position to the other players with passion! Player 🐱 can assist you.',
+            'Wear one of your panties on your forehead for one round!',
+            'Write a perverted message to player 🐰 like an 1800 style letter. Try to cause player 🐰 to blush!'
+        ]
+    },
+    l5: {
+        rabbit: [
+            'Any player who has never recorded something sexual on camera has to take a sip.',
+            'Tell the other players how you take care of your pubic hair!',
+            'Choose a different player to spank your butt!',
+            'Demonstrate your blow job technique to the other players with a fruit!',
+            'Tell the other players what you usually think about when touching yourself!',
+            'Sit on player 🦁\'s lap and act as father and daughter!',
+            'Take your shirt or top off, for the duration of one round!',
+            'Turn around and guess what kind of sexual practice the other players engage in behind your back.',
+            'Describe the scent of player 🦁\'s penis to the other players. Figure out a way to experience the scent.',
+            'The other players can decide what you have to do.'
+        ],
+        cat: [
+            'Have you ever experienced pain while having sex? Talk about it!',
+            'Any player who has never experienced an orgasm has to take a sip.',
+            'Demonstrate to the other players if you can open player 🦁\'s trousers and zipper with your mouth!',
+            'Drink if you have ever asked a man to cum onto you!',
+            'Take a really sexy picture of player 🐰! A picture that you would not share publicly.',
+            'Kiss player 🦁 slowly from bellybutton down to the pants!',
+            'Change one piece of your current clothing with one of player 🦁!',
+            'Explain what you would do to make player 🦁 cum as fast as possible!',
+            'Give a live demonstration on player 🦁 on how to put a condom on.',
+            'The other players can decide what you have to do.'
+        ],
+        lion: [
+            'Any player who has ever used food in a kinky way (either alone or with a partner) has to take a sip.',
+            'Demonstrate that you can recognise the other players by their smell, blindfolded!',
+            'All the players have to describe the strangest place they had to pee at in the past. You decide which is the strangest.',
+            'Any player who has never done something sexual in the open air has to take a sip.',
+            'Which part of player 🐱\'s body is the sexiest?',
+            'Demonstrate to the other players how you would comfort a girl at her first time having sex.',
+            'Put a picture of your penis among three others. The other players have to find it.',
+            'Rank the demonstrated blow job skills of the other players. They have to drink according to their rank.',
+            'Hide your penis with a party hat. The other players have to through rings on the party hat.',
+            'Which part of player 🐰\'s body is the sexiest?'
+        ]
+    }
+};
